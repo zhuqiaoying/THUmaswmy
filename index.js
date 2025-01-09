359b17daf21cf2114af2bd778546d27f9aabdd2c
Milmon <milmon@qq.com>
2025-01-09 12:03:09 +0800

    support download source code

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ const superagent = require('superagent');
 
 const TARGET_USER = 'thomaswmy';
 const data = fs.existsSync('data.json') ? JSON.parse(fs.readFileSync('data.json').toString()) : [];
+const cookie = JSON.parse(fs.readFileSync('secret.json'));
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function downloadSubmission(submissionId, retry = 30) {
+    if (fs.existsSync(`submissions/${submissionId}.json`) && fs.existsSync(`submissions/197421440.json`)) return;
+    console.info(`Downloading submission ${submissionId}`);
+    try {
+        const { body: { currentData: { record } } } = await superagent.get(`https://www.luogu.com.cn/record/${submissionId}`)
+            .query({ _contentOnly: true })
+            .set('Cookie', cookie);
+        record.user = record.user.name;
+        if (record.sourceCode) {
+            fs.writeFileSync(`submissions/${submissionId}.source`, record.sourceCode);
+            record.sourceCode = null;
+        }
+        fs.writeFileSync(`submissions/${submissionId}.json`, JSON.stringify(record));
+        await sleep(3000);
+    }
+    catch (err) {
+        console.error(err);
+        await sleep(3000);
+        downloadSubmission(submissionId, retry - 1)
+    }
+}
 
 async function main() {
     for (let page = 1; ; page++) {
@@ -15,7 +38,7 @@ async function main() {
                 page,
                 _contentOnly: true,
             })
-            .set('Cookie', JSON.parse(fs.readFileSync('secret.json')));
+            .set('Cookie', cookie);
         if (body.code !== 200) throw new Error('Status is not 200');
         const records = body.currentData.records.result;
         let newRecord = false, duplicate = false;
@@ -24,10 +47,11 @@ async function main() {
             if (data.find((r) => r.id === rdoc.id)) duplicate = true;
             else newRecord = true, data.push(rdoc);
         }
-        fs.writeFileSync('data.json', JSON.stringify(data, null, '  ').sort((x, y) => x.id - y.id));
+        fs.writeFileSync('data.json', JSON.stringify(data.sort((x, y) => x.id - y.id), null, '  '));
         if (!newRecord || duplicate) break;
         await sleep(3000);
     }
+    for (const rdoc of data) await downloadSubmission(rdoc.id);
 }
 
 main();
--- submissions/1.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b8755223c938dcd3c75bfc97fb170e1dba47c8c20f6046dc1709e47321e3129e
-size 5
